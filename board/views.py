c23c49daf29fc8492b26bccdc485744eb38eaa23
Xiorc <jshan1128@gmail.com>
2018-05-24 16:07:30 +0900

    Modified templates to display wiriting from database.

--- board/views.py
+++ board/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Board
 
 # Create your views here.
+def bulletin_list(request):
+	boards = Board.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'board/bulletin_list.html', {'boards': boards})
+
